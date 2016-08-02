a23466cc4ef61a5a79bfc9fc59d27ad6e854728f
David Feng <davidxifeng@gmail.com>
2016-08-03 04:40:55 +0800

    read elf section header

--- lualib/binary.lua
+++ lualib/binary.lua
@@ -5,13 +5,24 @@ local read_buf = buffer.read
 
 local M = {}
 
-M.read_buf = read_buf
-
-function M.read_struct(buf, info, fmt)
-  local res = read_buf(buf, assert(info.fmt or fmt))
+local function read_struct(buf, info, fmt)
+  local res = read_buf(buf, assert(fmt or info.fmt))
   assert(# res == # info)
   local r = {}
   for i, v in ipairs(info) do r[v] = res[i] end
+  return r
+end
+
+M.read_buf = read_buf
+M.read_struct = read_struct
+
+function M.read_array(buf, offset, size, num, info, fmt)
+  offset = offset - size
+  local fmt = '+%d %s'
+  local r = {}
+  for i = 1, num do
+    r[i] = read_struct(buf, info, fmt:format(offset + i * size, info.fmt))
+  end
   return r
 end
 
--- lualib/elf.lua
+++ lualib/elf.lua
@@ -8,12 +8,14 @@ local binary = require 'binary'
 
 local read_buf    = binary.read_buf
 local read_struct = binary.read_struct
+local read_array  = binary.read_array
+
 
 local Elf32_Ehdr = {
 }
 
 local Elf64_Ehdr = {
-  fmt = '{c16 u2 u2 u4 u8 u8 u8 u4 u2 u2 u2 u2 u2 u2}',
+  fmt = '< {c16 u2 u2 u4 u8 u8 u8 u4 u2 u2 u2 u2 u2 u2}',
   [1]  = 'ident',       -- ident bytes       16 bytes
   [2]  = 'type',        -- file type         Elf64_Half
   [3]  = 'machine',     -- target machine    Elf64_Half
@@ -30,6 +32,20 @@ local Elf64_Ehdr = {
   [14] = 'shstrndx',    -- shdr string index Elf64_Half
 }
 
+local Elf64_Shdr = {
+  fmt = '< {u4 u4 u8 u8 u8 u8 u4 u4 u8 u8}',
+  [1]  = 'sh_name',      -- Elf64_Word  section name
+  [2]  = 'sh_type',      -- Elf64_Word  SHT_...
+  [3]  = 'sh_flags',     -- Elf64_Xword SHF_...
+  [4]  = 'sh_addr',      -- Elf64_Addr  virtual address
+  [5]  = 'sh_offset',    -- Elf64_Off   file offset
+  [6]  = 'sh_size',      -- Elf64_Xword section size
+  [7]  = 'sh_link',      -- Elf64_Word  misc info
+  [8]  = 'sh_info',      -- Elf64_Word  misc info
+  [9]  = 'sh_addralign', -- Elf64_Xword memory alignment
+  [10] = 'sh_entsize',   -- Elf64_Xword entry size if table
+}
+
 local ElfMethods = {}
 
 --- 解析elf文件
@@ -39,15 +55,28 @@ function ElfMethods:parse()
   if mag ~= '\x7fELF' then return nil, 'bad elf magic' end
   self[2] = {} -- info table
 
-  if cls == 1 then
-    self[2].bit = '32-bit'
-    self[2].header = read_struct(buf, Elf32_Ehdr)
-  elseif cls == 2 then
-    self[2].bit = '64-bit'
-    self[2].header = read_struct(buf, Elf64_Ehdr)
-  else
-    return nil, 'unknown file class'
+  if     cls == 1 then return self:parse_elf32(buf)
+  elseif cls == 2 then return self:parse_elf64(buf)
+  else return nil, 'unknown file class'
   end
+end
+
+function ElfMethods:parse_elf32(buf)
+  local info = self[2]
+  info.bit = '32-bit'
+  info.header = read_struct(buf, Elf32_Ehdr)
+  return self
+end
+
+function ElfMethods:parse_elf64(buf)
+  local info = self[2]
+  info.bit = '64-bit'
+
+  local ehdr = read_struct(buf, Elf64_Ehdr)
+  info.header = ehdr
+
+  local shdr = read_array(buf, ehdr.shoff, ehdr.shentsize, ehdr.shnum, Elf64_Shdr)
+  info.shdr = shdr
 
   return self
 end
